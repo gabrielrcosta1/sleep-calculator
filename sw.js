4cfd5f2e65def4b3c3396c0bc66f5bab8178ec16
gabrielrcosta1 <contatogrsoftwares@gmail.com>
2024-10-16 12:43:52 -0400

    feat: convert app to PWA

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'sleep-calculator-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png',
+  './css/styles.css' // Adicione todos os recursos que você deseja cachear
+];
+
+// Instala o service worker e faz o cache dos arquivos
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Ativa o service worker e remove caches antigos, se necessário
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Intercepta as requisições de rede e responde com recursos em cache
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
+  );
+});
